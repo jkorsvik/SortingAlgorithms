88c28d55cdd1c66651851ce4d0cd91d9b5150b6b
Jon-Mikkel Korsvik <jonkors@nmbu.no>
2020-10-27 15:20:00 +0100

    Added data and iterative versions of QS and MS

---
+++ notebooks/generate_data_script.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+import os
+import sys
+from pathlib import Path, PurePath
+sys.path.append("..")
+# For quicksort
+sys.setrecursionlimit(int(1e7))
+
+from src.linear_sorting_algorithms import radixsort
+from src.quadratic_sorting_algorithms import bubble_sort, insertion_sort
+from src.subquad_sorting_algorithms import quicksort, mergesort, iterative_quicksort
+from src.combined_sorting_algorithm import mergesort_combined
+
+from src.utility import time_sorting_algorithms, ArrayGenerator
+
+if Path('../data').exists():
+    data_directory = PurePath("../data")
+    print(data_directory)
+
+array_gen = ArrayGenerator(seed=12)
+
+# N for 2-base generator, N^2
+N = 14
+cols = {"Ascending" : np.single, "Descending" : np.single, "Random" : np.single, "Structured" : np.single, "Integers" :np.int32}
+
+test_data = dict()
+
+for i in range(1, N+1):
+    data = np.array([array_gen.sorted_array(i),
+            array_gen.reversed_array(i),
+            array_gen.random_array(i),
+            array_gen.structured_array(i),
+            array_gen.integer_array(i)
+           ])
+    test_data[i] = pd.DataFrame(columns=cols.keys(), data=data.T).astype(cols)
+
+list_of_algorithms = [bubble_sort, insertion_sort, iterative_quicksort, mergesort, mergesort_combined]
+
+
+df_results = pd.DataFrame(columns=["Algorithm", "2^N", "TypeArray", "Time"])
+
+for N, Array in test_data.items():
+    for TypeArray in cols.keys():
+        
+        for algorithm, times in time_sorting_algorithms(
+            functions=list_of_algorithms, 
+            array=Array[TypeArray].to_numpy(), 
+            iters=5, 
+            verbose=True
+        ).items():
+            
+            for time in times:
+                
+                df_results = df_results.append(
+                    {
+                    "Algorithm": algorithm, "N^2": N, "TypeArray": TypeArray, "Time": time
+                    }, 
+                    ignore_index=True
+                )
+        
+
+df_results.to_csv(f"{data_directory}/benchmark_results_test.csv")
--- src/subquad_sorting_algorithms.py
+++ src/subquad_sorting_algorithms.py
@@ -34,7 +34,7 @@ def mergesort(A: np.array or list):
             copy_index += 1
           
         # Checking if elements are remaining in Left or Right
-        # left_array is copied first since indexing(copy_index) goes from left to right
+        # left_array is copartionining_indexed first since indexing(copy_index) goes from left to right
         while left_index < len(left_array): 
 
             A[copy_index] = left_array[left_index] 
@@ -48,6 +48,28 @@ def mergesort(A: np.array or list):
             copy_index += 1
 
 
+def mergesort_iterative(A: np.array or list):
+
+    new_list = map(lambda x: [x], A)
+    theLength = 1
+
+    while theLength < len(A):
+        temp_list = []
+
+        pairs = zip(new_list[::2], new_list[1::2])
+
+        for pair in pairs:
+            temp_list.append( mergesort_iterative( pair[0], pair[1] ) )
+
+        if len(pairs) * 2 < len(new_list):
+            temp_list.append(new_list[-1])
+
+        theLength *= 2
+        new_list = temp_list
+
+    return temp_list[0]
+
+
 
 
 def swap(array, a, b):
@@ -56,16 +78,16 @@ def swap(array, a, b):
 
 def partition(array, start, end):
     
-    pivotindex = start
-    pivotvalue = array[end]
+    pivot_index = start
+    pivot_value = array[end]
 
     for left_index in range(start,end):
-        if array[left_index] < pivotvalue:
-            swap(array, left_index, pivotindex)
-            pivotindex += 1
-    swap(array, pivotindex, end)
+        if array[left_index] < pivot_value:
+            swap(array, left_index, pivot_index)
+            pivot_index += 1
+    swap(array, pivot_index, end)
     
-    return array, pivotindex
+    return array, pivot_index
 
 def quick_sort(array, start, end):
     if start < end:
@@ -79,6 +101,71 @@ def quicksort(array):
     array = quick_sort(array, 0, len(array)-1)
 
 
+
+
+
+def iterative_quicksort(array):
+    quickSort_iterative(array, 0, len(array) - 1)
+
+
+def iter_partition(arr, low, high): 
+    i = ( low - 1 ) 
+    x = arr[high] 
+  
+    for j in range(low, high): 
+        if   arr[j] <= x: 
+  
+            # increment index of smaller element 
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i] 
+  
+    arr[i + 1], arr[high] = arr[high], arr[i + 1] 
+    return (i + 1) 
+  
+
+def quickSort_iterative(arr, low, high): 
+  
+    # Create an auxiliary stack 
+    size = high - low + 1
+    stack = [0] * (size) 
+  
+    # initialize top of stack 
+    top = -1
+  
+    # push initial values of low and high to stack 
+    top = top + 1
+    stack[top] = low 
+    top = top + 1
+    stack[top] = high 
+  
+    # Keep popping from stack while is not empty 
+    while top >= 0: 
+  
+        # Pop high and low 
+        high = stack[top] 
+        top = top - 1
+        low = stack[top] 
+        top = top - 1
+  
+        # Set pivot element at its correct position in 
+        # sorted array 
+        p = iter_partition(arr, low, high) 
+  
+        # If there are elements on left side of pivot, 
+        # then push left side to stack 
+        if p-1 > low: 
+            top = top + 1
+            stack[top] = low 
+            top = top + 1
+            stack[top] = p - 1
+  
+        # If there are elements on right side of pivot, 
+        # then push right side to stack 
+        if p + 1 < high: 
+            top = top + 1
+            stack[top] = p + 1
+            top = top + 1
+            stack[top] = high 
 
 
 
