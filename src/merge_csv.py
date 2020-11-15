c89eb86dcc8ed771885b25223234a3e3575ef716
Jon-Mikkel Korsvik <jonkors@nmbu.no>
2020-11-15 21:13:52 +0100

    Optimized and finished merging of csv, gen of data

--- src/merge_csv.py
+++ src/merge_csv.py
@@ -2,36 +2,40 @@
 """
 
 import pandas as pd
-import numpy as np
-
 import os
 import sys
 from pathlib import Path, PurePath
 sys.path.append("..")
-# For quicksort
-sys.setrecursionlimit(int(1e7))
-
 
 if Path('../data').exists():
     data_directory = PurePath("../data")
+    print(data_directory)
+
+if Path('./data').exists():
+    data_directory = PurePath("./data")
     print(data_directory)
 
 def merge_csv(result_filename="merged_results"):
     df = None
     for data_file in os.listdir(data_directory):
         if data_file.endswith(".csv"):
-            if df is not None:
+
+            if data_file == result_filename:
+                continue # Skips merging previous merges
+
+            if df is None:
                 df = pd.read_csv(os.path.join(data_directory, data_file))
             else:
                 temp = pd.read_csv(os.path.join(data_directory, data_file))
-                df = pd.concat([df, temp], axis=0, ignore_index=True)
+                df = df.append(temp, ignore_index=True)
         else:
             continue
     print(f"Merged all Dataframes to shape {df.shape}")
     result_filename += ".csv"
-    prompt = input(f"Do you want to save the result as {result_filename}: Y/[n]")
-    df.drop_duplicates()
-    if prompt.lower().__contains__(y):
+
+    prompt = input(f"Do you want to save the result as {result_filename} Y/[n]: ")
+    df.drop_duplicates(subset=df.columns)
+    if prompt.lower().__contains__("y"):
         df.to_csv(os.path.join(data_directory, result_filename))
         print(f"csv was saved in {data_directory}")
 
